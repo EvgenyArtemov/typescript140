1ca116dddb7afd3ee3cc45cd5485ef2d6b4f893e
Evgeny Artemov <evgeny.artemov@cteleport.com>
2024-09-26 07:30:23 +0400

    2. Pick<> implementation

---
+++ 00004-easy-pick.ts
@@ -0,0 +1,32 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
+  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
+  // @ts-expect-error
+  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
+]
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+}
+
+interface Expected2 {
+  title: string
+  completed: boolean
+}
+
+
+// ============= Your Code Here =============
+// it's important to add generic constraints
+// this way we don't need to handle that on lower levels
+type MyPick<T extends {}, K extends keyof T> = {
+  [P in K]: T[P]
+}
