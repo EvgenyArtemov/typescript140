7e3b83b733dc53ea2e4724c4d39fc1add8eec541
Evgeny Artemov <eartemov013@gmail.com>
2025-01-27 17:21:05 +0400

    33. Flatten

---
+++ 00459-medium-flatten.ts
@@ -0,0 +1,24 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Flatten<[]>, []>>,
+  Expect<Equal<Flatten<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
+  Expect<Equal<Flatten<[{ foo: 'bar'; 2: 10 }, 'foobar']>, [{ foo: 'bar'; 2: 10 }, 'foobar']>>,
+]
+
+// @ts-expect-error
+type error = Flatten<'1'>
+
+type Test = Flatten<[1, 2, [3, 4], [[[5]]]]>
+
+
+// ============= Your Code Here =============
+type Flatten<T extends unknown[], Acc extends unknown[] = []> =
+    T extends [infer Head, ...infer Tail]
+      ? Head extends unknown[]
+        ? Flatten<[...Head, ...Tail], Acc>
+        : Flatten<[...Tail], [...Acc, Head]>
+      : Acc
